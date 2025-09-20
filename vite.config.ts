068ad978bb4ccf40ffb2fd4646742b2918054bdc
LSN-Ram <ramakrishnan.java95@gmail.com>
2025-09-21 00:14:48 +0530

    git ignore changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -7,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,11 +17,9 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
- 
-  base: "/",
+  base: "./", 
 });
